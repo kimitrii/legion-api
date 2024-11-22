bf5f41ab4d6a78133b61527749778a7234442012
Kimitri <kimitrimusic@gmail.com>
2024-11-22 17:11:57 -0300

    feat: add user to D1 sample endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { Hono } from 'hono'
+import { drizzle } from 'drizzle-orm/d1';
+import { Env } from './types/drizzleTypes';
+import { users } from './db/schema';
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Env}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  const db = drizzle(c.env.DB)
+
+  await db.insert(users).values({
+    name: 'Eric',
+  })
+
+  const dbUsers = await db.select().from(users)
+
+  return c.json(dbUsers[0])
 })
 
 export default app
