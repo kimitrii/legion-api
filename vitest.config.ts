2e25671b11f0bc580609a45d436968291a9d3c10
Kimitri <kimitrimusic@gmail.com>
2024-11-25 05:22:31 -0300

    chore: remove wrangler.toml from Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,28 @@
-import { defineWorkersConfig, readD1Migrations } from "@cloudflare/vitest-pool-workers/config";
+import {
+	defineWorkersConfig,
+	readD1Migrations
+} from '@cloudflare/vitest-pool-workers/config'
 
 export default defineWorkersConfig(async () => {
-  const migrations = await readD1Migrations("./src/migrations")
-  return {
-    test: {
-      globals: true,
-      poolOptions: {
-        workers: {
-          isolatedStorage: true,
-          miniflare: {
-            bindings: {
-              TEST_MIGRATIONS: migrations
-            }
-          },
-          wrangler: {
-            configPath: "./wrangler.toml"
-          }
-        }
-      }
-    }
-  }
+	const migrations = await readD1Migrations('./src/migrations')
+	return {
+		test: {
+			globals: true,
+			poolOptions: {
+				workers: {
+					isolatedStorage: true,
+					miniflare: {
+						d1Databases: {
+							DB: 'db-tests'
+						},
+						bindings: {
+							TEST_MIGRATIONS: migrations
+						},
+						compatibilityDate: '2024-11-20',
+						compatibilityFlags: ['nodejs_compat']
+					}
+				}
+			}
+		}
+	}
 })
