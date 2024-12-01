faedf1f550344caed18337be353b6ed47839633c
Kimitri <kimitrimusic@gmail.com>
2024-12-01 02:26:38 -0300

    refactor: rename drizzleTypes.ts to bindTypes.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 import usersRouters from './routers/users.router'
-import type { Env } from './types/drizzleTypes'
+import type { Env } from './types/bindTypes'
 
 const app = new Hono<{ Bindings: Env }>()
 
--- src/types/drizzleTypes.ts
+++
@@ -1,3 +0,0 @@
-export type Env = {
-    DB: D1Database
-  }
