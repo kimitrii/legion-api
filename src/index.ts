63671cb24a3be6dc9258be184cf702351a835c35
Kimitri <kimitrimusic@gmail.com>
2025-01-09 00:51:57 -0300

    feat: add CSRF and secureHeaders midleware protection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Hono } from 'hono'
+import { csrf } from 'hono/csrf'
+import { secureHeaders } from 'hono/secure-headers'
 import { errorsHandler } from './handlers/error/Errors.Handler'
 import usersRouters from './routers/users.router'
 
 const app = new Hono()
+
+app.use(csrf({ origin: process.env.LEGION_WEBSITE }))
+
+app.use(secureHeaders())
 
 app.route('/', usersRouters)
 
