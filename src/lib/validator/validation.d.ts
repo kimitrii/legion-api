f0c316fdf3895945e320be8055050d40a802e720
Kimitri <kimitrimusic@gmail.com>
2025-01-09 09:44:00 -0300

    refactor: add partial generic support in check method and return field

--- src/lib/validator/validation.d.ts
+++ src/lib/validator/validation.d.ts
@@ -1,5 +1,5 @@
 type ValidationResult =
 	| { success: true }
-	| { success: false; failedValidator: string }
+	| { success: false; failedValidator: string; field?: string }
 
 export type ValidatorFunction = (value: T) => ValidationResult
