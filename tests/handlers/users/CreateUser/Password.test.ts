81aa0e13af256a35de0c4ad5c5af0c3e12753a73
Kimitri <kimitrimusic@gmail.com>
2025-01-12 15:22:18 -0300

    test(users): add password encryption cases

---
+++ tests/handlers/users/CreateUser/Password.test.ts
@@ -0,0 +1,55 @@
+import { applyD1Migrations, env } from 'cloudflare:test'
+import { users } from '@src/db/user.schema'
+import app from '@src/index'
+import bcrypt from 'bcryptjs'
+import { eq } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/d1'
+import { afterEach, beforeAll, describe, expect, test } from 'vitest'
+
+describe('User password encryption - E2E', () => {
+	const db = drizzle(env.DB)
+
+	const headers = {
+		'Content-Type': 'application/json',
+		'X-CSRF-Token': 'mock-csrf-token'
+	}
+
+	beforeAll(async () => {
+		await applyD1Migrations(env.DB, env.TEST_MIGRATIONS)
+	})
+
+	afterEach(async () => {
+		await db.delete(users)
+	})
+
+	test('should save encrypted password in database with bcrypt', async () => {
+		const payload = JSON.stringify({
+			name: 'John Doe',
+			username: 'johndoe123',
+			password: 'secureP@ssw0rd!',
+			email: 'johndoe@example.com'
+		})
+
+		await app.request(
+			'/users',
+			{
+				method: 'POST',
+				headers,
+				body: payload
+			},
+			env
+		)
+
+		const [user] = await db
+			.select()
+			.from(users)
+			.where(eq(users.email, 'johndoe@example.com'))
+			.limit(1)
+
+		expect(await bcrypt.compare('secureP@ssw0rd!', user.password ?? '')).toBe(
+			true
+		)
+		expect(user.password).toMatch(/^\$2[ayb]\$.{56}$/)
+		expect(user.password).not.toBe('secureP@ssw0rd!')
+	})
+})
