de730b65aafc2e4b8f82d4ea576115e0842565d5
Kimitri <kimitrimusic@gmail.com>
2025-01-14 00:40:22 -0300

    feat(users): add strict validation to createUserSchema

--- src/validations/users/CreateUser.Validation.ts
+++ src/validations/users/CreateUser.Validation.ts
@@ -1,8 +1,13 @@
 import { validator } from '@src/lib/validator'
 
-export const createUserSchema = validator.schema({
-	name: validator.string().max(256).min(1),
-	username: validator.string().max(256).min(1),
-	password: validator.string().max(256).min(8).nullable(),
-	email: validator.string().email().max(256).nullable()
-})
+export const createUserSchema = validator.schema(
+	{
+		name: validator.string().max(256).min(1),
+		username: validator.string().max(256).min(1),
+		password: validator.string().max(256).min(8).nullable(),
+		email: validator.string().email().max(256).nullable()
+	},
+	{
+		strict: true
+	}
+)
