e2537964e0ee968927c40b0571e485a856d00d91
Kimitri <kimitrimusic@gmail.com>
2025-01-17 23:00:42 -0300

    feat(users): implement DeleteUserHandler router

---
+++ src/handlers/users/DeleteUser/DeleteUser.Handler.ts
@@ -0,0 +1,31 @@
+import type { User } from '@src/entities/User.Entity'
+import { UserRepository } from '@src/repositories/users/User.Repository'
+import { DeleteUserService } from '@src/services/users/DeleteUser/DeleteUser.Service'
+import type { Presenter } from '@src/types/presenter'
+import { factory } from '@src/utils/factory'
+import type { TypedResponse } from 'hono'
+import { logger } from 'hono/logger'
+import type { StatusCode } from 'hono/utils/http-status'
+
+export const DeleteUserHandler = factory.createHandlers(
+	logger(),
+	async (c): Promise<TypedResponse<Presenter<User>, StatusCode>> => {
+		const usersRepository = new UserRepository(c.env.DB)
+		const deleteUserService = new DeleteUserService(usersRepository)
+
+		const id = c.req.param('id')
+
+		const data = { id }
+
+		const user = await deleteUserService.execute(data)
+
+		return c.json(
+			{
+				success: true,
+				message: 'User deleted successfully',
+				data: user
+			},
+			200
+		)
+	}
+)
--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,4 +1,5 @@
 import { CreateUserHandler } from '@src/handlers/users/CreateUser/CreateUser.Handler'
+import { DeleteUserHandler } from '@src/handlers/users/DeleteUser/DeleteUser.Handler'
 import { UpdateUserHandler } from '@src/handlers/users/UpdateUser/UpdateUser.Handler'
 import { Hono } from 'hono'
 
@@ -6,5 +7,6 @@ const usersRouters = new Hono()
 
 usersRouters.post('/users', ...CreateUserHandler)
 usersRouters.put('/users/:id', ...UpdateUserHandler)
+usersRouters.delete('/users/:id', ...DeleteUserHandler)
 
 export default usersRouters
