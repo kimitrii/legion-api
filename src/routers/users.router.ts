6b3939e7b9ffe37ad489c8280a698fe279f2a926
Kimitri <kimitrimusic@gmail.com>
2025-01-18 12:27:35 -0300

    feat(users): implement GetUserById handler

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,10 +1,12 @@
 import { CreateUserHandler } from '@src/handlers/users/CreateUser/CreateUser.Handler'
 import { DeleteUserHandler } from '@src/handlers/users/DeleteUser/DeleteUser.Handler'
+import { GetUserByIdHandler } from '@src/handlers/users/GetUserById/GetUserById.Handler'
 import { UpdateUserHandler } from '@src/handlers/users/UpdateUser/UpdateUser.Handler'
 import { Hono } from 'hono'
 
 const usersRouters = new Hono()
 
+usersRouters.get('/users/:id', ...GetUserByIdHandler)
 usersRouters.post('/users', ...CreateUserHandler)
 usersRouters.put('/users/:id', ...UpdateUserHandler)
 usersRouters.delete('/users/:id', ...DeleteUserHandler)
