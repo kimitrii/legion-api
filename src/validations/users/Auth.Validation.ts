fa0a53b1dc5a39a4b5730dd043f415ea674c61de
Kimitri <kimitrimusic@gmail.com>
2025-01-23 00:28:05 -0300

    feat(auth): implement JWT authentication service with email/username and password

---
+++ src/validations/users/Auth.Validation.ts
@@ -0,0 +1,12 @@
+import { validator } from '@src/lib/validator'
+
+export const authSchema = validator.schema(
+	{
+		username: validator.string().max(256).min(1).nullable(),
+		password: validator.string().max(256).min(8),
+		email: validator.string().email().max(256).nullable()
+	},
+	{
+		strict: true
+	}
+)
