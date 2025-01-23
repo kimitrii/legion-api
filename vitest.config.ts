dbe5a7d46f2f26c819b0997f5104216b164476e7
Kimitri <kimitrimusic@gmail.com>
2025-01-23 15:09:25 -0300

    chore(Vitest): add AUTH_ISSUER to bindings variables

--- vitest.config.ts
+++ vitest.config.ts
@@ -28,7 +28,8 @@ export default defineWorkersConfig(async () => {
 						bindings: {
 							TEST_MIGRATIONS: migrations,
 							USER_SECRET_KEY: process.env.USER_SECRET_KEY ?? '',
-							REFRESH_SECRET_KEY: process.env.REFRESH_SECRET_KEY ?? ''
+							REFRESH_SECRET_KEY: process.env.REFRESH_SECRET_KEY ?? '',
+							AUTH_ISSUER: process.env.AUTH_ISSUER ?? ''
 						},
 						compatibilityDate: '2024-11-20',
 						compatibilityFlags: ['nodejs_compat']
