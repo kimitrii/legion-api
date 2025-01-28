88fab0713bbd40b0cc299f716d4d72571fc3fb5e
Kimitri <kimitrimusic@gmail.com>
2025-01-28 17:41:36 -0300

    feat(createUserSchema): add isTotpEnable to create user schema validation

--- src/validations/users/CreateUser.Validation.ts
+++ src/validations/users/CreateUser.Validation.ts
@@ -5,7 +5,8 @@ export const createUserSchema = validator.schema(
 		name: validator.string().max(256).min(1),
 		username: validator.string().max(256).min(1),
 		password: validator.string().max(256).min(8).nullable(),
-		email: validator.string().email().max(256).nullable()
+		email: validator.string().email().max(256).nullable(),
+		isTotpEnable: validator.boolean()
 	},
 	{
 		strict: true
