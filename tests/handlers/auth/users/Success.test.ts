2369328014abff7a0e9f2b8004923c076f3e115b
Kimitri <kimitrimusic@gmail.com>
2025-01-28 16:21:07 -0300

    test(UserAuth): add rehash password on successfully aunthentication

--- tests/handlers/auth/users/Success.test.ts
+++ tests/handlers/auth/users/Success.test.ts
@@ -2,6 +2,7 @@ import { applyD1Migrations, env } from 'cloudflare:test'
 import { users } from '@src/db/user.schema'
 import app from '@src/index'
 import bcrypt from 'bcryptjs'
+import { eq } from 'drizzle-orm'
 import { drizzle } from 'drizzle-orm/d1'
 import { afterEach, beforeAll, describe, expect, test } from 'vitest'
 
@@ -127,5 +128,67 @@ describe('User authentication handler E2E', () => {
 				}
 			}
 		})
+	})
+
+	test('should should rehash password on successfully authentication', async () => {
+		const encryptedPassword = await bcrypt.hash('secureP@ssw0rd!', 2)
+
+		const date = new Date().toISOString()
+		await db.insert(users).values({
+			id: '01JHBDWAXFPAKAFK38E1MAM01W',
+			name: 'John Doe',
+			username: 'johndoe123',
+			password: encryptedPassword,
+			email: 'johndoe@example.com',
+			isActive: true,
+			deletedAt: null,
+			kats: 0,
+			rank: 0,
+			createdAt: date
+		})
+
+		const payload = JSON.stringify({
+			email: 'johndoe@example.com',
+			password: 'secureP@ssw0rd!'
+		})
+
+		const res = await app.request(
+			'/users/login',
+			{
+				method: 'POST',
+				headers,
+				body: payload
+			},
+			env
+		)
+
+		const result = await res.json()
+
+		const jwtRegex = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
+
+		const rehashedUser = await db
+			.select()
+			.from(users)
+			.where(eq(users.id, '01JHBDWAXFPAKAFK38E1MAM01W'))
+			.limit(1)
+
+		expect(res.status).toBe(200)
+		expect(result).toStrictEqual({
+			success: true,
+			message: 'User authenticated successfully',
+			data: {
+				id: '01JHBDWAXFPAKAFK38E1MAM01W',
+				name: 'John Doe',
+				username: 'johndoe123',
+				email: 'johndoe@example.com',
+				token: {
+					accessToken: expect.stringMatching(jwtRegex),
+					refreshToken: expect.stringMatching(jwtRegex),
+					expiresIn: expect.any(Number)
+				}
+			}
+		})
+
+		expect(bcrypt.getRounds(rehashedUser[0].password ?? '')).toBe(10)
 	})
 })
