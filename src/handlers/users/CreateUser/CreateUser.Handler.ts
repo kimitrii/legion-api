15c392bacae37fc0a707327fc7e742f035ac1013
Kimitri <kimitrimusic@gmail.com>
2025-01-30 14:19:28 -0300

    feat(CreateUserHandler): add OTP repository and inject OTP_SECRET in service

--- src/handlers/users/CreateUser/CreateUser.Handler.ts
+++ src/handlers/users/CreateUser/CreateUser.Handler.ts
@@ -1,5 +1,6 @@
-import type { User } from '@src/entities/User.Entity'
+import type { ISanitizedUserDTO } from '@src/dtos/User.DTO'
 import { AppError } from '@src/errors/AppErrors.Error'
+import { OtpRepository } from '@src/repositories/auth/Otp.DTO'
 import { UserRepository } from '@src/repositories/users/User.Repository'
 import { CreateUserService } from '@src/services/users/CreateUser/CreateUser.Service'
 import type { Presenter } from '@src/types/presenter'
@@ -10,9 +11,16 @@ import type { StatusCode } from 'hono/utils/http-status'
 
 export const CreateUserHandler = factory.createHandlers(
 	logger(),
-	async (c): Promise<TypedResponse<Presenter<User>, StatusCode>> => {
+	async (
+		c
+	): Promise<TypedResponse<Presenter<ISanitizedUserDTO>, StatusCode>> => {
 		const usersRepository = new UserRepository(c.env.DB)
-		const createUserService = new CreateUserService(usersRepository)
+		const otpsRepository = new OtpRepository(c.env.DB)
+		const createUserService = new CreateUserService(
+			usersRepository,
+			otpsRepository,
+			c.env.OTP_SECRET
+		)
 
 		const data = await c.req.json().catch(() => {
 			throw new AppError({
