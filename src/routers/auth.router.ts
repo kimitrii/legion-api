2f2743ff121b1cea76519a11f2403e72865e1712
Kimitri <kimitrimusic@gmail.com>
2025-02-03 01:59:09 -0300

    feat(authRouters): add enable otp router

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,8 +1,10 @@
+import { EnableOtpHandler } from '@src/handlers/auth/users/EnableOTP.Handler'
 import { UserAuthHandler } from '@src/handlers/auth/users/UserAuth.Handler'
 import { Hono } from 'hono'
 
 const authRouters = new Hono()
 
 authRouters.post('/users/login', ...UserAuthHandler)
+authRouters.post('/users/:id/otp/verify', ...EnableOtpHandler)
 
 export default authRouters
