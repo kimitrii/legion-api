f56ca7da5f142cc06605edfd8b82eb0ccdce1405
Kimitri <kimitrimusic@gmail.com>
2025-02-03 02:00:52 -0300

    test(EnableOTP): add not found failure cases

---
+++ tests/handlers/auth/enableOTP/NotFound.failure.test.ts
@@ -0,0 +1,85 @@
+import { applyD1Migrations, env } from 'cloudflare:test'
+import { users } from '@src/db/user.schema'
+import app from '@src/index'
+import { drizzle } from 'drizzle-orm/d1'
+import { afterEach, beforeAll, describe, expect, test } from 'vitest'
+
+describe('Enable user otp handler E2E', () => {
+	const db = drizzle(env.DB)
+
+	const headers = {
+		'Content-Type': 'application/json',
+		'X-CSRF-Token': 'mock-csrf-token'
+	}
+
+	beforeAll(async () => {
+		await applyD1Migrations(env.DB, env.TEST_MIGRATIONS)
+	})
+
+	afterEach(async () => {
+		await db.delete(users)
+	})
+
+	test('should fail if not otp found', async () => {
+		const date = new Date().toISOString()
+
+		await db.insert(users).values({
+			id: '01JHBDWAXFPAKAFK38E1MAM01W',
+			name: 'John Doe',
+			username: 'johndoe123',
+			email: 'johndoe@example.com',
+			isTotpEnable: false,
+			isActive: true,
+			deletedAt: null,
+			kats: 0,
+			rank: 0,
+			createdAt: date
+		})
+
+		const payload = JSON.stringify({
+			token: '465456'
+		})
+
+		const res = await app.request(
+			'/users/01JHBDWAXFPAKAFK38E1MAM01W/otp/verify',
+			{
+				method: 'POST',
+				headers,
+				body: payload
+			},
+			env
+		)
+
+		const result = await res.json()
+
+		expect(res.status).toBe(404)
+		expect(result).toStrictEqual({
+			success: false,
+			message: 'No OTP found'
+		})
+	})
+
+	test('should fail if not user found', async () => {
+		const payload = JSON.stringify({
+			token: '465456'
+		})
+
+		const res = await app.request(
+			'/users/01JHBDWAXFPAKAFK38E1MAM01W/otp/verify',
+			{
+				method: 'POST',
+				headers,
+				body: payload
+			},
+			env
+		)
+
+		const result = await res.json()
+
+		expect(res.status).toBe(404)
+		expect(result).toStrictEqual({
+			success: false,
+			message: 'No OTP found'
+		})
+	})
+})
