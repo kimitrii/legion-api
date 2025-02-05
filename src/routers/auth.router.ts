6f44964df910399df8ec51455fcc90a1c30941e7
Kimitri <kimitrimusic@gmail.com>
2025-02-05 13:35:13 -0300

    feat(authRouters): add generate otp url router

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,10 +1,17 @@
 import { EnableOtpHandler } from '@src/handlers/auth/users/EnableOTP.Handler'
+import { GenerateOtpAuthUrlHandler } from '@src/handlers/auth/users/GenerateOtpAuth.Handler'
 import { UserAuthHandler } from '@src/handlers/auth/users/UserAuth.Handler'
+import { authenticateToken } from '@src/middleware/Auth.middleware'
 import { Hono } from 'hono'
 
 const authRouters = new Hono()
 
 authRouters.post('/users/login', ...UserAuthHandler)
 authRouters.put('/users/:id/otp', ...EnableOtpHandler)
+authRouters.post(
+	'/users/:id/otp/secret',
+	authenticateToken,
+	...GenerateOtpAuthUrlHandler
+)
 
 export default authRouters
