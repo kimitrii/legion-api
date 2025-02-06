d9cf372f693be1fb4ce8a7d7e7cd990fba098472
Kimitri <kimitrimusic@gmail.com>
2025-02-06 14:31:40 -0300

    feat(OtpRepository): add findManyByOr to search by mutiple filters

--- src/repositories/auth/Otp.Repository.ts
+++ src/repositories/auth/Otp.Repository.ts
@@ -2,10 +2,11 @@ import { otps } from '@src/db/otp.schema'
 import { users } from '@src/db/user.schema'
 import type { IOtpDTO } from '@src/dtos/Otp.DTO'
 import type {} from '@src/dtos/Pagination.DTO'
+import type { IFindByParamsDTO } from '@src/dtos/Repositories.DTO'
 import type { Otp } from '@src/entities/Otp.Entity'
 import type { User } from '@src/entities/User.Entity'
 import { AppError } from '@src/errors/AppErrors.Error'
-import { desc, eq } from 'drizzle-orm'
+import { and, desc, eq } from 'drizzle-orm'
 import { type DrizzleD1Database, drizzle } from 'drizzle-orm/d1'
 
 export class OtpRepository {
@@ -17,14 +18,26 @@ export class OtpRepository {
 		this.db = drizzle(D1)
 	}
 
-	public async findByUser(data: { id: string }): Promise<
-		(User & { otps: Otp[] }) | null
-	> {
+	public async findManyByOr(
+		data: IFindByParamsDTO
+	): Promise<(User & { otps: Otp[] }) | null> {
+		const includeConditions = []
+
+		if (data.id) {
+			includeConditions.push(eq(otps.userId, data.id))
+		}
+		if (data.email) {
+			includeConditions.push(eq(users.email, data.email))
+		}
+		if (data.username) {
+			includeConditions.push(eq(users.username, data.username))
+		}
+
 		const process = await this.db
 			.select()
 			.from(users)
 			.innerJoin(otps, eq(users.id, otps.userId))
-			.where(eq(otps.userId, data.id))
+			.where(and(...includeConditions))
 			.orderBy(desc(otps.createdAt))
 
 		if (process.length === 0) {
