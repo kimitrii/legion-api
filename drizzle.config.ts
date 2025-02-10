1cc0999a0eecbd7b2d9d07e0eb4769fd80824b62
Kimitri <kimitrimusic@gmail.com>
2025-02-10 01:31:47 -0300

    feat(database): add refreshToken table with users relation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
 	dialect: 'sqlite',
-	schema: ['./src/db/user.schema.ts', './src/db/otp.schema.ts'],
+	schema: [
+		'./src/db/user.schema.ts',
+		'./src/db/otp.schema.ts',
+		'./src/db/refreshToken.schema.ts'
+	],
 	out: './src/migrations'
 })
